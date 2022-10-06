1ea523ae9f21764339c88e1a25a9d9ce98d5701b
Carlos Mantilla <carlos_wilfrido666@hotmail.com>
2022-10-06 15:28:50 -0500

    fix: reformateo de código

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { EmployeeService } from './services/employee.service';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
